2949347b43066627272fcf74c36d22d5e5d1b27c
Malte Mosbach <maltemosbach@gmail.com>
2023-01-08 20:45:52 +0100

    Update project page

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+let papertitle = "Efficient Representations of Object Geometry for Reinforcement Learning of Interactive Grasping Policies"
+
+// set papertitle
+document.getElementById("papertitle_website").innerHTML = papertitle;
+document.getElementById("papertitle_heading").innerHTML = papertitle;
